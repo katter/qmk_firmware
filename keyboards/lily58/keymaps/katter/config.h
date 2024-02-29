947e871e01bcee875ce4b55f4f7c059f77bbf9a3
Carsten Hiort <carsten.hiort@gmail.com>
2024-02-29 12:07:06 +0100

    Running Config

--- keyboards/lily58/keymaps/katter/config.h
+++ keyboards/lily58/keymaps/katter/config.h
@@ -48,7 +48,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
 
 // Underglow
 /*
